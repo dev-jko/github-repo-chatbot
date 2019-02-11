433146dc1563fc8d6aecb475072b8eeb5016c701
nadarm <31630051+nadarm@users.noreply.github.com>
2019-02-11 19:48:49 +0900

    Feature/send msg (#29)
    * ss3 | messenger | ~190131
    
    * home | slack msg search | ~190202
    
    * home | change name sender -> handler | 190202
    
    * home | searcher can read private channel | ~190210
    
    * ss3 | add print log | ~190211

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template, request
+import requests
+import os
+import msg_handler
+import time
+from datetime import datetime
 
 app = Flask(__name__)
 
@@ -13,5 +18,51 @@ def test(string):
     return(f'test {string}!')
 
 
+@app.route('/send_msg', methods=['POST', 'GET'])
+def send_msg():
+    if request.method == 'GET':
+        return render_template('msg.html')
+    else:
+        msg = msg_handler
+        if request.form['messenger'] == 'slack':
+            SLACK_BOT_TOKEN = os.getenv('SLACK_BOT_TOKEN')
+            print(SLACK_BOT_TOKEN)
+            sender = msg.SlackMsgSender(SLACK_BOT_TOKEN, 'wizard')
+        else:
+            TELEGRAM_MY_ID = os.getenv('TELEGRAM_MY_ID')
+            TELEGRAM_BOT_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
+            sender = msg.TelegramMsgSender(TELEGRAM_BOT_TOKEN, TELEGRAM_MY_ID)
+        if sender.send_msg(request.form['msg']):
+            ok = '성공'
+        else:
+            ok = '실패'
+        return render_template('msg.html', ok=ok)
+
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    # app.run(debug=True)
+
+    msg = msg_handler
+    SLACK_BOT_TOKEN = os.getenv('SLACK_BOT_TOKEN')
+    SLACK_SEARCH_TOKEN = os.getenv('SLACK_SEARCH_TOKEN')
+    SLACK_GENERAL_ID = os.getenv('SLACK_GENERAL_ID')
+    SLACK_WIZARD_ID = os.getenv('SLACK_WIZARD_ID')
+
+    seacher = msg.SlackMsgSearcher(SLACK_SEARCH_TOKEN)
+    sender = msg.SlackMsgSender(SLACK_BOT_TOKEN, 'wizard')
+
+    COMMANDS = {'\너굴맨', '\점심', '\날씨'}
+
+    while True:
+        # commands = seacher.search_msg(COMMANDS, SLACK_GENERAL_ID)
+        commands = seacher.search_msg(
+            COMMANDS, SLACK_WIZARD_ID, is_private=True)
+        print(f'{datetime.now()} - find - {commands}')
+        for command in commands:
+            if command == '\너굴맨':
+                sender.send_msg('조너굴 바보', 'general')
+            if command == '\점심':
+                sender.send_msg('점심 메뉴')
+            if command == '\날씨':
+                sender.send_msg('날씨')
+        time.sleep(1)
---
+++ msg_handler.py
@@ -0,0 +1,86 @@
+import requests
+import os
+import datetime
+import time
+
+
+class MsgSearcher:
+    def __init__(self, bot_token):
+        self.bot_token = bot_token
+        self.timestamp = time.time()
+
+
+class SlackMsgSearcher(MsgSearcher):
+    # use slack search api
+    def search_msg2(self, msg):
+        url = f'https://slack.com/api/search.messages?token={self.bot_token}&query={msg}'
+        response = requests.get(url).json()
+        if response['ok']:
+            messages = response['messages']['matches']
+            result_set = []
+            temp_timestamp = self.timestamp
+            for message in messages:
+                message_time = float(message['ts'])
+                if message_time > self.timestamp:
+                    if msg in message['text']:
+                        temp_timestamp = message_time
+                        result = {}
+                        result['channel'] = message['channel']['name']
+                        result['channel_id'] = message['channel']['id']
+                        result_set.append(result)
+            self.timestamp = temp_timestamp
+            return result_set
+        else:
+            return None
+
+    # use slack channel history api
+    def search_msg(self, msg_set, channel_id, is_private=False):
+        url = f'https://slack.com/api/channels.history?token={self.bot_token}&channel={channel_id}&count=5'
+        if is_private:
+            url = f'https://slack.com/api/groups.history?token={self.bot_token}&channel={channel_id}&count=5'
+        response = requests.get(url).json()
+        result = set({})
+        if response['ok']:
+            messages = response['messages']
+            temp_timestamp = self.timestamp
+            for message in messages:
+                message_time = float(message['ts'])
+                if message_time > self.timestamp:
+                    for msg in msg_set:
+                        if msg in message['text']:
+                            result.add(msg)
+                            temp_timestamp = message_time
+                            self.timestamp = temp_timestamp
+            self.timestamp = temp_timestamp
+        return result
+
+
+class MsgSender:
+    def __init__(self, bot_token):
+        self.bot_token = bot_token
+
+
+class TelegramMsgSender(MsgSender):
+    def __init__(self, bot_token, receive_user):
+        super().__init__(bot_token)
+        self.receive_user = receive_user
+
+    def send_msg(self, msg, receive_user=None):
+        if receive_user is None:
+            receive_user = self.receive_user
+        url = f'https://api.telegram.org/bot{self.bot_token}/sendMessage?chat_id={receive_user}&text={msg}'
+        response = requests.post(url).json()
+        return response['ok']
+
+
+class SlackMsgSender(MsgSender):
+    def __init__(self, bot_token, channel):
+        super().__init__(bot_token)
+        self.channel = channel
+
+    def send_msg(self, msg, channel=None):
+        if channel is None:
+            channel = self.channel
+        url = f'https://slack.com/api/chat.postMessage?token={self.bot_token}&channel={channel}&text={msg}&pretty=1'
+        response = requests.post(url).json()
+        return response['ok']
